e755598303d0fd02a9b0daff02dde6bb989f0788
Matthias Walthart <coding@dschinghis-kahn.net>
2023-01-12 22:41:25 +0100

    Replaced logger with system.outs.

--- src/test/java/net/dschinghiskahn/server/ServerTest.java
+++ src/test/java/net/dschinghiskahn/server/ServerTest.java
@@ -13,14 +13,9 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import net.dschinghiskahn.objectdecoupler.IObjectReceiver;
@@ -30,292 +25,286 @@ import net.dschinghiskahn.server.net.UdpNetworkData;
 
 public class ServerTest implements IObjectReceiver<INetworkData> {
 
-    public static final int PORT = 50000;
-    public static final String HOSTNAME = "localhost";
-    private Server server;
-    private INetworkData networkData;
-    private String read;
+	public static final int PORT = 50000;
+	public static final String HOSTNAME = "localhost";
+	private Server server;
+	private INetworkData networkData;
+	private String read;
 
-    @Override
-    public void receiveObject(INetworkData networkData) {
-        this.networkData = networkData;
-    }
+	@Override
+	public void receiveObject(INetworkData networkData) {
+		this.networkData = networkData;
+	}
 
-    public String getRead() {
-        return read;
-    }
+	public String getRead() {
+		return read;
+	}
 
-    public void setRead(String read) {
-        this.read = read;
-    }
+	public void setRead(String read) {
+		this.read = read;
+	}
 
-    @BeforeClass
-    public static void init() {
-        Logger.getRootLogger().setLevel(Level.INFO);
-        Logger.getRootLogger().removeAllAppenders();
-        ConsoleAppender appender = new ConsoleAppender();
-        appender.setLayout(new PatternLayout("%d %-5p: %m%n"));
-        appender.activateOptions();
-        Logger.getRootLogger().addAppender(appender);
-    }
+	@Before
+	public void before() throws IOException, NoSuchMethodException {
+		setRead(null);
+		server = new Server(HOSTNAME, PORT);
+		server.registerSocketReceiver(this);
+	}
 
-    @Before
-    public void before() throws IOException, NoSuchMethodException {
-        setRead(null);
-        server = new Server(HOSTNAME, PORT);
-        server.registerSocketReceiver(this);
-    }
+	@After
+	public void after() {
+		if (server != null) {
+			try {
+				server.shutdown();
+			} catch (IOException e) {
+				System.err.println(getClass().getSimpleName()+" - Server threw an exception during shutdown!\n" + e.getLocalizedMessage());
+			}
+		}
+	}
 
-    @After
-    public void after() {
-        if (server != null) {
-            server.shutdown();
-        }
-    }
+	@Test(timeout = 1000)
+	public void stop() throws IOException {
+		System.out.println(getClass().getSimpleName()+" - Running test: stop()");
+		server.shutdown();
+		for (Thread thread : Thread.getAllStackTraces().keySet()) {
+			Assert.assertFalse(thread.getName().equals("ServerWorker0"));
+		}
+	}
 
-    @Test(timeout = 1000)
-    public void stop() {
-        Logger.getLogger(getClass()).info("Running test: stop()");
-        server.shutdown();
-        for (Thread thread : Thread.getAllStackTraces().keySet()) {
-            Assert.assertFalse(thread.getName().equals("ServerWorker0"));
-        }
-    }
-
-    @Test(timeout = 1000)
-    public void connection() {
-        Logger.getLogger(getClass()).info("Running test: connection()");
-        boolean result = true;
-        Socket socket = null;
-        try {
-            socket = new Socket(HOSTNAME, PORT);
-        } catch (UnknownHostException e) {
-            result = false;
-        } catch (IOException e) {
-            result = false;
-        } finally {
-        	if(socket != null) {
-        		try {
+	@Test(timeout = 1000)
+	public void connection() {
+		System.out.println(getClass().getSimpleName()+" - Running test: connection()");
+		boolean result = true;
+		Socket socket = null;
+		try {
+			socket = new Socket(HOSTNAME, PORT);
+		} catch (UnknownHostException e) {
+			result = false;
+		} catch (IOException e) {
+			result = false;
+		} finally {
+			if (socket != null) {
+				try {
 					socket.close();
 				} catch (IOException e) {
 					// Intentionally left empty.
 				}
-        	}
-        }
-        Assert.assertTrue(result);
-    }
+			}
+		}
+		Assert.assertTrue(result);
+	}
 
-    @Test(timeout = 5000)
-    public void manyConnections() {
-        Logger.getLogger(getClass()).info("Running test: manyConnections()");
-        boolean result = true;
-        Socket socket = null;
-        try {
-            for (int i = 0; i < 1000; i++) {
-            	socket = new Socket(HOSTNAME, PORT);
-            }
-        } catch (UnknownHostException e) {
-            result = false;
-        } catch (IOException e) {
-            result = false;
-        } finally {
-        	if(socket != null) {
-        		try {
+	@Test(timeout = 5000)
+	public void manyConnections() {
+		System.out.println(getClass().getSimpleName()+" - Running test: manyConnections()");
+		boolean result = true;
+		Socket socket = null;
+		try {
+			for (int i = 0; i < 1000; i++) {
+				socket = new Socket(HOSTNAME, PORT);
+			}
+		} catch (UnknownHostException e) {
+			result = false;
+		} catch (IOException e) {
+			result = false;
+		} finally {
+			if (socket != null) {
+				try {
 					socket.close();
 				} catch (IOException e) {
 					// Intentionally left empty.
 				}
-        	}
-        }
-        Assert.assertTrue(result);
-    }
+			}
+		}
+		Assert.assertTrue(result);
+	}
 
-    @Test(timeout = 3000)
-    public void wrongPort() {
-        Logger.getLogger(getClass()).info("Running test: wrongPort()");
-        Socket socket = null;
-        try {
-        	socket = new Socket(HOSTNAME, PORT + 1);
-        } catch (IOException e) {
-            Assert.assertTrue(true);
-            return;
-        } finally {
-        	if(socket != null) {
-        		try {
+	@Test(timeout = 3000)
+	public void wrongPort() {
+		System.out.println(getClass().getSimpleName()+" - Running test: wrongPort()");
+		Socket socket = null;
+		try {
+			socket = new Socket(HOSTNAME, PORT + 1);
+		} catch (IOException e) {
+			Assert.assertTrue(true);
+			return;
+		} finally {
+			if (socket != null) {
+				try {
 					socket.close();
 				} catch (IOException e) {
 					// Intentionally left empty.
 				}
-        	}
-        }
-        Assert.assertTrue(false);
-    }
+			}
+		}
+		Assert.assertTrue(false);
+	}
 
-    @Test(timeout = 1000)
-    public void wrongUrl() {
-        Logger.getLogger(getClass()).info("Running test: wrongUrl()");
-        Socket socket = null;
-        try {
-        	socket = new Socket("192.168.1.", PORT);
-        } catch (UnknownHostException e) {
-            Assert.assertTrue(true);
-            return;
-        } catch (IOException e) {
-            Assert.assertTrue(false);
-        } finally {
-        	if(socket != null) {
-        		try {
+	@Test(timeout = 1000)
+	public void wrongUrl() {
+		System.out.println(getClass().getSimpleName()+" - Running test: wrongUrl()");
+		Socket socket = null;
+		try {
+			socket = new Socket("192.168.1.", PORT);
+		} catch (UnknownHostException e) {
+			Assert.assertTrue(true);
+			return;
+		} catch (IOException e) {
+			Assert.assertTrue(false);
+		} finally {
+			if (socket != null) {
+				try {
 					socket.close();
 				} catch (IOException e) {
 					// Intentionally left empty.
 				}
-        	}
-        }
-        Assert.assertTrue(false);
-    }
+			}
+		}
+		Assert.assertTrue(false);
+	}
 
-    @Test(timeout = 1000)
-    public void udpConnectionType() throws InterruptedException, IOException {
-        Logger.getLogger(getClass()).info("Running test: udpConnectionType()");
-        DatagramSocket socket = new DatagramSocket();
-        DatagramPacket packet = new DatagramPacket("test".getBytes(), 4, InetAddress.getByName(HOSTNAME), PORT);
-        socket.send(packet);
+	@Test(timeout = 1000)
+	public void udpConnectionType() throws InterruptedException, IOException {
+		System.out.println(getClass().getSimpleName()+" - Running test: udpConnectionType()");
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket packet = new DatagramPacket("test".getBytes(), 4, InetAddress.getByName(HOSTNAME), PORT);
+		socket.send(packet);
 
-        while (networkData == null) {
-            Thread.sleep(1);
-        }
+		while (networkData == null) {
+			Thread.sleep(1);
+		}
 
-        Assert.assertTrue(networkData instanceof UdpNetworkData);
-        Assert.assertFalse(networkData instanceof TcpNetworkData);
-        
-        socket.close();
-    }
+		Assert.assertTrue(networkData instanceof UdpNetworkData);
+		Assert.assertFalse(networkData instanceof TcpNetworkData);
 
-    @Test(timeout = 1000)
-    public void tcpConnectionType() throws InterruptedException, IOException {
-        Logger.getLogger(getClass()).info("Running test: tcpConnectionType()");
-        Socket socket = new Socket(HOSTNAME, PORT);
+		socket.close();
+	}
 
-        while (networkData == null) {
-            Thread.sleep(1);
-        }
+	@Test(timeout = 1000)
+	public void tcpConnectionType() throws InterruptedException, IOException {
+		System.out.println(getClass().getSimpleName()+" - Running test: tcpConnectionType()");
+		Socket socket = new Socket(HOSTNAME, PORT);
 
-        Assert.assertTrue(networkData instanceof TcpNetworkData);
-        Assert.assertFalse(networkData instanceof UdpNetworkData);
-        
-        socket.close();
-    }
+		while (networkData == null) {
+			Thread.sleep(1);
+		}
 
-    @Test(timeout = 5000)
-    public void shutdown() throws IOException {
-        Logger.getLogger(getClass()).info("Running test: shutdown()");
-        server.shutdown();
-        server = new Server(HOSTNAME, PORT, 100);
-        server.shutdown();
-    }
+		Assert.assertTrue(networkData instanceof TcpNetworkData);
+		Assert.assertFalse(networkData instanceof UdpNetworkData);
 
-    @Test(timeout = 1000)
-    public void basicTcpTransfer() throws IOException, InterruptedException {
-        Logger.getLogger(getClass()).info("Running test: basicTcpTransfer()");
-        Socket socket = new Socket(HOSTNAME, PORT);
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(15);
-        outputStream.flush();
+		socket.close();
+	}
 
-        while (networkData == null) {
-            Thread.sleep(1);
-        }
+	@Test(timeout = 5000)
+	public void shutdown() throws IOException {
+		System.out.println(getClass().getSimpleName()+" - Running test: shutdown()");
+		server.shutdown();
+		server = new Server(HOSTNAME, PORT, 100);
+		server.shutdown();
+	}
 
-        Assert.assertTrue(networkData instanceof TcpNetworkData);
+	@Test(timeout = 1000)
+	public void basicTcpTransfer() throws IOException, InterruptedException {
+		System.out.println(getClass().getSimpleName()+" - Running test: basicTcpTransfer()");
+		Socket socket = new Socket(HOSTNAME, PORT);
+		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write(15);
+		outputStream.flush();
 
-        InputStream inputStream = ((TcpNetworkData) networkData).getTcpInputStream();
-        int result = inputStream.read();
-        Assert.assertEquals(15, result);
-        
-        socket.close();
-    }
+		while (networkData == null) {
+			Thread.sleep(1);
+		}
 
-    @Test(timeout = 1000)
-    public void dataTcpTransfer() throws IOException, InterruptedException {
-        Logger.getLogger(getClass()).info("Running test: dataTcpTransfer()");
-        Socket socket = new Socket(HOSTNAME, PORT);
-        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-        outputStream.write(15);
-        outputStream.flush();
+		Assert.assertTrue(networkData instanceof TcpNetworkData);
 
-        while (networkData == null) {
-            Thread.sleep(1);
-        }
+		InputStream inputStream = ((TcpNetworkData) networkData).getTcpInputStream();
+		int result = inputStream.read();
+		Assert.assertEquals(15, result);
 
-        Assert.assertTrue(networkData instanceof TcpNetworkData);
+		socket.close();
+	}
 
-        DataInputStream inputStream = new DataInputStream(((TcpNetworkData) networkData).getTcpInputStream());
-        int result = inputStream.read();
+	@Test(timeout = 1000)
+	public void dataTcpTransfer() throws IOException, InterruptedException {
+		System.out.println(getClass().getSimpleName()+" - Running test: dataTcpTransfer()");
+		Socket socket = new Socket(HOSTNAME, PORT);
+		DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+		outputStream.write(15);
+		outputStream.flush();
 
-        Assert.assertTrue(result == 15);
-        
-        socket.close();
-    }
+		while (networkData == null) {
+			Thread.sleep(1);
+		}
 
-    @Test(timeout = 1000)
-    public void objectTcpTransfer() throws IOException, InterruptedException, ClassNotFoundException {
-        Logger.getLogger(getClass()).info("Running test: objectTcpTransfer()");
-        Socket socket = new Socket(HOSTNAME, PORT);
-        ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-        outputStream.writeObject("15");
-        outputStream.flush();
+		Assert.assertTrue(networkData instanceof TcpNetworkData);
 
-        while (networkData == null) {
-            Thread.sleep(1);
-        }
+		DataInputStream inputStream = new DataInputStream(((TcpNetworkData) networkData).getTcpInputStream());
+		int result = inputStream.read();
 
-        Assert.assertTrue(networkData instanceof TcpNetworkData);
+		Assert.assertTrue(result == 15);
 
-        ObjectInputStream inputStream = new ObjectInputStream(((TcpNetworkData) networkData).getTcpInputStream());
-        String result = (String) inputStream.readObject();
+		socket.close();
+	}
 
-        Assert.assertTrue("15".equals(result));
-        
-        socket.close();
-    }
+	@Test(timeout = 1000)
+	public void objectTcpTransfer() throws IOException, InterruptedException, ClassNotFoundException {
+		System.out.println(getClass().getSimpleName()+" - Running test: objectTcpTransfer()");
+		Socket socket = new Socket(HOSTNAME, PORT);
+		ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+		outputStream.writeObject("15");
+		outputStream.flush();
 
-    @Test(timeout = 1000)
-    public void simultaneousReadWrite() throws IOException, InterruptedException {
-        Logger.getLogger(getClass()).info("Running test: simultaneousReadWrite()");
-        Socket socket = new Socket(HOSTNAME, PORT);
-        OutputStream outputStream = socket.getOutputStream();
-        InputStream inputStream = socket.getInputStream();
+		while (networkData == null) {
+			Thread.sleep(1);
+		}
 
-        DataOutputStream localOutput = new DataOutputStream(outputStream);
-        DataInputStream localInput = new DataInputStream(inputStream);
+		Assert.assertTrue(networkData instanceof TcpNetworkData);
 
-        new ReadThread(localInput, this).start();
+		ObjectInputStream inputStream = new ObjectInputStream(((TcpNetworkData) networkData).getTcpInputStream());
+		String result = (String) inputStream.readObject();
 
-        while (networkData == null) {
-            Thread.sleep(10);
-        }
+		Assert.assertTrue("15".equals(result));
 
-        DataOutputStream remoteOutput = new DataOutputStream(((TcpNetworkData) networkData).getTcpOutputStream());
-        DataInputStream remoteInput = new DataInputStream(((TcpNetworkData) networkData).getTcpInputStream());
+		socket.close();
+	}
 
-        new ReadThread(remoteInput, this).start();
+	@Test(timeout = 1000)
+	public void simultaneousReadWrite() throws IOException, InterruptedException {
+		System.out.println(getClass().getSimpleName()+" - Running test: simultaneousReadWrite()");
+		Socket socket = new Socket(HOSTNAME, PORT);
+		OutputStream outputStream = socket.getOutputStream();
+		InputStream inputStream = socket.getInputStream();
 
-        localOutput.writeUTF("local_test");
+		DataOutputStream localOutput = new DataOutputStream(outputStream);
+		DataInputStream localInput = new DataInputStream(inputStream);
 
-        while (getRead() == null) {
-            Thread.sleep(10);
-        }
-        Assert.assertTrue(getRead().equals("local_test"));
+		new ReadThread(localInput, this).start();
 
-        setRead(null);
-        remoteOutput.writeUTF("remote_test");
+		while (networkData == null) {
+			Thread.sleep(10);
+		}
 
-        while (getRead() == null) {
-            Thread.sleep(10);
-        }
-        Assert.assertTrue(getRead().equals("remote_test"));
-        
-        socket.close();
-    }
+		DataOutputStream remoteOutput = new DataOutputStream(((TcpNetworkData) networkData).getTcpOutputStream());
+		DataInputStream remoteInput = new DataInputStream(((TcpNetworkData) networkData).getTcpInputStream());
+
+		new ReadThread(remoteInput, this).start();
+
+		localOutput.writeUTF("local_test");
+
+		while (getRead() == null) {
+			Thread.sleep(10);
+		}
+		Assert.assertTrue(getRead().equals("local_test"));
+
+		setRead(null);
+		remoteOutput.writeUTF("remote_test");
+
+		while (getRead() == null) {
+			Thread.sleep(10);
+		}
+		Assert.assertTrue(getRead().equals("remote_test"));
+
+		socket.close();
+	}
 
 }
